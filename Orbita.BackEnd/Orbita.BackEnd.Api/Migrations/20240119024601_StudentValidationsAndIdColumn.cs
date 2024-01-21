6a1fee78946d6edce7cb3d8ad4d9db0156d28d94
Murilo Fernandes Melo <murilofm2010@gmail.com>
2024-01-20 23:06:52 -0300

    Student validations and Id column

---
+++ Orbita.BackEnd/Orbita.BackEnd.Api/Migrations/20240119024601_StudentValidationsAndIdColumn.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Orbita.BackEnd.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentValidationsAndIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Students",
+                type: "char(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Students",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_RA",
+                table: "Students",
+                column: "RA",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_RA",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "char(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "RA");
+        }
+    }
+}
