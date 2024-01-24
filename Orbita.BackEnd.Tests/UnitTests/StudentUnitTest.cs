3c7511cfd55b94d36daba47421a3d4261e503801
Murilo Fernandes Melo <murilofm2010@gmail.com>
2024-01-24 02:26:20 -0300

    Tests to new student rules

--- Orbita.BackEnd.Tests/UnitTests/StudentUnitTest.cs
+++ Orbita.BackEnd.Tests/UnitTests/StudentUnitTest.cs
@@ -22,19 +22,16 @@ namespace Orbita.BackEnd.Tests.UnitTests
             Assert.True(result);
         }
 
-
         [Fact]
-        public void CreateStudent_WithInvalidCPF_ShouldBeInvalid()
+        public void CreateStudent_WithInvalidCPFLenght_ShouldBeInvalid()
         {
             var student = new Student { Name = "Raissa Gonçalves", CPF = "45641", Email = "raissa@gmail.com", RA = "24023214" };
 
             var context = new ValidationContext(student);
-            var teste = context.Items;
             var result = Validator.TryValidateObject(student, context, null, true);
 
             Assert.False(result);
         }
-
 
         [Fact]
         public void CreateStudent_WithInvalidEmail_ShouldBeInvalid()
@@ -42,7 +39,17 @@ namespace Orbita.BackEnd.Tests.UnitTests
             var student = new Student { Name = "Raissa Gonçalves", CPF = "10987654321", Email = "hellothisisnotaemail", RA = "24023214" };
 
             var context = new ValidationContext(student);
-            var teste = context.Items;
+            var result = Validator.TryValidateObject(student, context, null, true);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CreateStudent_WithInvalidCPFWithLetters_ShouldBeInvalid()
+        {
+            var student = new Student { Name = "Raissa Gonçalves", CPF = "10987as3211", Email = "hellothisisnotaemail", RA = "24023214" };
+
+            var context = new ValidationContext(student);
             var result = Validator.TryValidateObject(student, context, null, true);
 
             Assert.False(result);
